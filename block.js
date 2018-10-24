300d60599bca08bd3f6eb443b936b52d8fea9ad3
Cijams <cijams@asu.edu>
2018-10-23 19:21:49 -0700

    Added First block + nodemon
    Added the constructor to the first block. Continued and made the toString to get live feedback from nodemon. Created the dev-test.js script. Lastly touched up JSON.

--- block.js
+++ block.js
@@ -1,8 +1,19 @@
 class Block {
-	constructory(timestamp, lastHash, hash, data) {
+	
+	constructor(timestamp, lastHash, hash, data) {
 		this.timestamp = timestamp;
 		this.lastHash = lastHash;
 		this.hash = hash;
 		this.data = data;
 	}
+
+	toString() {
+        return `Block -
+        	Timestamp: ${this.timestamp}
+        	Last Hash: ${this.lastHash}
+        	Hash     : ${this.hash}
+        	Data     : ${this.data}`;
+    }
 }
+
+module.exports = Block;
